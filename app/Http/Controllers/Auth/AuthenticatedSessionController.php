45166e0490e685999330caed0aa7c556ef5c041b
Yusuf  Maruf <achmadusufalmaruf@gmail.com>
2024-06-01 12:41:49 +0700

    starter template UI

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended(route('dashboard', absolute: false))->with('success_message', 'Selamat datang ' . Auth::user()->name);
     }
 
     /**
