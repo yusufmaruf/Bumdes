2fb3bb0be8cee4333a9319fc564ac279464a6a42
Yusuf  Maruf <achmadusufalmaruf@gmail.com>
2024-06-05 23:35:10 +0700

    sosial medi controller

---
+++ resources/views/layouts/pages/admin/SosialMedia/tombol.blade.php
@@ -0,0 +1,6 @@
+<div class="btn-group">
+    <a class="btn  btn-warning btn-flat"
+        href="{{ route('admin.sosialmedia.edit', ['sosialmedia' => $data->idSiteIdentity]) }}">
+        Sunting
+    </a>
+</div>
